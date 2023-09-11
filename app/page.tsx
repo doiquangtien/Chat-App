4f019c3fcf49e8f68f35639f646604edd2c17b59
doiquangtien <qangtien27999@gmail.com>
2023-09-11 13:51:40 +0700

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <Button size="lg">Click</Button>
+      <p className="text-3xl font-bold">Hello World</p>
+    </div>
+  );
+}
